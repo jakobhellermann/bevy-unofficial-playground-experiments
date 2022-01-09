304d73a825a06807b46696e0522897dd05ada861
Jakob Hellermann <jakob.hellermann@protonmail.com>
2022-01-09 22:00:39 +0100

    bevy 0.6

--- bevy-playground-website/src/defaultSource.ts
+++ bevy-playground-website/src/defaultSource.ts
@@ -1,26 +1,14 @@
-export default `use bevy::{
-    ecs::prelude::*,
-    math::{Mat4, Quat, Vec3},
-    prelude::{App, Assets, BuildChildren, Transform},
-    render2::{
-        camera::{OrthographicProjection, PerspectiveCameraBundle},
-        color::Color,
-        mesh::{shape, Mesh},
-    },
-    PipelinedDefaultPlugins,
-};
+export default `use bevy::prelude::*;
 
 fn main() {
     App::new()
         .insert_resource(bevy::window::WindowDescriptor {
             canvas: Some("#bevy_canvas".to_string()),
-            // width: 768.0,
-            // height: 432.0,
-            width: 200.0,
-            height: 100.0,
+            width: 768.0,
+            height: 432.0,
             ..Default::default()
         })
-        .add_plugins(PipelinedDefaultPlugins)
+        .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .run();
 }
@@ -29,11 +17,34 @@ fn main() {
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
+    // cube
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..Default::default()
+    });
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..Default::default()
+    });
     // camera
     commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(0.0, 0.0, 4.0)
-            .looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
     });
 }`;
